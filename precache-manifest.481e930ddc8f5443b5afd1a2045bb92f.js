1cc3c1167b521553480661c5d98840548f6c76c7
lxg <479161553@qq.com>
2022-01-16 07:03:49 +0800

    deploy

---
+++ precache-manifest.481e930ddc8f5443b5afd1a2045bb92f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "634c0532bbb7dc85fc59ac0c19236049",
+    "url": "/longmen-serve/QRcode.png"
+  },
+  {
+    "revision": "94be396dee0dc55885c9",
+    "url": "/longmen-serve/css/app.a3ccd2c1.css"
+  },
+  {
+    "revision": "a952134dd07e4ae691572d0455b2b211",
+    "url": "/longmen-serve/dragon.ico"
+  },
+  {
+    "revision": "62802d35ebeaeef7ce48cdb621d4de55",
+    "url": "/longmen-serve/index.html"
+  },
+  {
+    "revision": "94be396dee0dc55885c9",
+    "url": "/longmen-serve/js/app.0337aa96.js"
+  },
+  {
+    "revision": "359bf4d5803d65ab853a",
+    "url": "/longmen-serve/js/chunk-vendors.963c6109.js"
+  },
+  {
+    "revision": "b4b15fb4873c486ef89893e4bd3eb20f",
+    "url": "/longmen-serve/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/longmen-serve/robots.txt"
+  }
+]);
